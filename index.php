9dcd6533938a2c54e07bfbec281f7c9d4e060118
92Chihiro <34203596+92Chihiro@users.noreply.github.com>
2018-05-16 22:41:57 +0900

    update index.php

--- index.php
+++ index.php
@@ -5,10 +5,10 @@ require 'vendor/autoload.php';
 use Abraham\TwitterOAuth\TwitterOAuth;
 
 $keyword = 'JustinBieber';
-$consumerKey = 'UhuBsj54DbRnnHGQYaPywHmAi';
-$consumerSecret = 'e1zSaGoQorVKzyW5W7U8LaAmkUsf3nnjdIHCxh45Y03bnnBj86';
-$accessToken = '42566916-5CI2BDtmiUQdWFKtTY6ajgxmLlYjURI4ftW2K88h3';
-$accessTokenSecret = 'YHfdFB1RJxIcHQ6XOriX0GZhmZmFpNmI5wegaBIRDflIU';
+$consumerKey = '';
+$consumerSecret = '';
+$accessToken = '';
+$accessTokenSecret = '';
 
 $conn = new TwitterOAuth($consumerKey,$consumerSecret,$accessToken,$accessTokenSecret);
 
